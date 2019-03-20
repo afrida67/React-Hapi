8f8f78b6cc57ef7e413e5640587c6bd75176812f
Afrida Aesha <afrida660@gmail.com>
2019-03-21 01:54:07 +0600

    GET requet done

--- client/src/components/StudentList.js
+++ client/src/components/StudentList.js
@@ -1,21 +1,54 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
-class StudentList extends Component {
 
+
+const Student = props => (
+  <tr>
+      <td>{props.student.username}</td>
+      <td>{props.student.name}</td>
+      <td>{props.student.email}</td>
+      <td>
+          <Link to={"/edit/"+props.student._id}>Edit</Link>
+      </td>
+  </tr>
+)
+class StudentList extends Component {
+      state = {
+        students: []
+      };
  
-  componentDidMount() {
-    axios.get('http://localhost:5000/')
-     .then(response => {
-            this.setState({todos: response.data});
-        })
-        .catch(function (error) {
-            console.log(error);
-        })
-}
+      componentDidMount() {
+        axios.get('http://localhost:5000/')
+            .then(response => {
+                this.setState({students: response.data});
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+    }
+    studentList() {
+      return this.state.students.map((currentStudent, i) => {
+          return <Student student={currentStudent} key={i} />;
+      });
+  }
   render() {
     return (
       <div>
-        Welcome to student list
+        <h3>Student List</h3>
+                <table className="table table-striped" style={{ marginTop: 20 }}>
+                    <thead>
+                        <tr>
+                            <th>Username</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        { this.studentList() }
+                    </tbody>
+                </table>
       </div>
     )
   }
